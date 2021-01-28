4ce9c2ec3d1b8d7bd9e53da1744274ad86053ff9
Tushar Jain <tusharjain.champ@gmail.com>
2021-01-28 00:23:48 +0000

    Fix Logging & Saving

--- bart/run.py
+++ bart/run.py
@@ -130,19 +130,19 @@ def train(args, logger, model, train_data, dev_data, optimizer, scheduler):
                         if wait_step >= args.wait_step:
                             stop_training = True
                             break
-                else:
-                    if global_step % args.log_period == 0:
-                        logger.info("Step %d (epoch %d) Train loss %.2f" % (
-                                global_step,
-                                epoch,
-                                np.mean(train_losses)))
-                        train_losses = []
-                    if global_step % args.save_period == 0:
-                        model_state_dict = {k:v.cpu() for (k, v) in model.state_dict().items()}
-                        if args.n_gpu > 1:
-                            model_state_dict = convert_to_single_gpu(model_state_dict)
-                        torch.save(model_state_dict, os.path.join(args.output_dir,
-                                                                    "best-model-{}.pt".format(str(global_step).zfill(6))))
+            else:
+                if global_step % args.log_period == 0:
+                    logger.info("Step %d (epoch %d) Train loss %.2f" % (
+                            global_step,
+                            epoch,
+                            np.mean(train_losses)))
+                    train_losses = []
+                if global_step % args.save_period == 0:
+                    model_state_dict = {k:v.cpu() for (k, v) in model.state_dict().items()}
+                    if args.n_gpu > 1:
+                        model_state_dict = convert_to_single_gpu(model_state_dict)
+                    torch.save(model_state_dict, os.path.join(args.output_dir,
+                                                                "best-model-{}.pt".format(str(global_step).zfill(6))))
                 model.train()
         if stop_training:
             break
